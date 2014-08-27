83a1db101c34942ad04c390cc8082b02afb6db68
Even Holthe <even.holthe@me.com>
2014-08-27 12:59:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Oblig 1 i AlgDat
+
+Dette er besvarelsen for Per Erik Finstad og Even Holthe (oblig1) i faget Algoritmer og datastrukturer
---
+++ src/main/java/no/acme/algdat/oblig1/Oblig1.java
@@ -0,0 +1,15 @@
+package no.acme.algdat.oblig1;
+
+/**
+ * Hello world!
+ *
+ */
+public class Oblig1
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+
+}

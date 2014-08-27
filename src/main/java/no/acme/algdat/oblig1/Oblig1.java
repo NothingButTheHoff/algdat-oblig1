be39143d91e74f1ede55ea7826f9473e5c433406
Even Holthe <even.holthe@me.com>
2014-08-27 15:23:16 +0200

    Løser oppgave 2. Fixes #2

--- src/main/java/no/acme/algdat/oblig1/Oblig1.java
+++ src/main/java/no/acme/algdat/oblig1/Oblig1.java
@@ -1,5 +1,6 @@
 package no.acme.algdat.oblig1;
 
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 /**
@@ -10,9 +11,10 @@ import java.util.NoSuchElementException;
 public class Oblig1
 {
     public static void main(String[] args){
-		int a[] = { 10, 9, 99, 8, 11, 9, 8, 5, 4, 3, 2, 1 };
-		System.out.println("Det største tallet i arrayet er: "+maks(a));
-    }
+		int a[] = { 15,5,4,8,12,3,6,13,17,2,12,10,17,5,13,5,9,20,11 };
+		sortering(a);
+		System.out.println(Arrays.toString(a));
+	}
 
 	// Oppgave 1, issue 1
 	public static int maks(int[] a){
@@ -22,17 +24,35 @@ public class Oblig1
 
 		int[] tall = a;
 
-		int n0, n1 = 0;
+		int temp = 0;
 
 		for(int i=0;i<tall.length-1;i++){
 			if(tall[i] > tall[i+1]){
-				n0 = tall[i];
-				n1 = tall[i+1];
-				tall[i] = n1;
-				tall[i+1] = n0;
+				temp = tall[i];
+				tall[i] = tall[i+1];
+				tall[i+1] = temp;
 			}
 		}
 
 		return tall[tall.length-1];
+	}
+
+	// Oppgave 2, issue 2
+	public static void sortering(int[] a){
+		if(a.length < 2) {
+			return;
+		}
+
+		int temp = 0;
+
+		for(int i=a.length;i>0;i--){
+			for(int j=0; j<i-1; j++){
+				if(a[j] > a[j+1]){
+					temp = a[j];
+					a[j] = a[j + 1];
+					a[j + 1] = temp;
+				}
+			}
+		}
 	}
 }
